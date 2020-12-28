1352948baef5bde2ea98ff711d205407ff8b3dc8
윤성민 <house0909min@gmail.com>
2020-12-28 17:22:11 +0900

    bot upload

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ client.on("message", (message) => {
     return message.reply("pong")
   }
 
-  if (message.content == "블루문") {
+  if (message.content == "blue") {
     let img = "https://cdn.discordapp.com/icons/419671192857739264/6dccc22df4cb0051b50548627f36c09b.webp?size=256"
     let embed = new Discord.MessageEmbed()
       .setTitle("블루문")
